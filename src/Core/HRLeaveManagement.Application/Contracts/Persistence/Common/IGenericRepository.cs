bea4f46084008ab06cf4a42b28c15e07e5dd4bc2
Ahmet Altun <ahmet.altun60@gmail.com>
2023-01-23 11:48:49 +0300

    Created Application layer and added to Generic Repository Pattern

---
+++ src/Core/HRLeaveManagement.Application/Contracts/Persistence/Common/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace HRLeaveManagement.Application.Contracts.Persistence.Common;
+
+public interface IGenericRepository<T> where T: class, new()
+{
+    Task<bool> CreateAsync(T entity, CancellationToken cancellationToken);
+    Task<bool> UpdateAsync(T entity, CancellationToken cancellationToken);
+    Task<bool> RemoveAsync(T entity, CancellationToken cancellationToken);
+    Task<T?> GetAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken);
+    Task<T?> GetByIdAsync(Guid Id, CancellationToken cancellationToken); 
+    Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken,
+        Expression<Func<T, bool>>? expression = null);
+}
---
+++ src/Core/HRLeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
@@ -0,0 +1,9 @@
+﻿using HRLeaveManagement.Application.Contracts.Persistence.Common;
+using HRLeaveManagement.Domain;
+
+namespace HRLeaveManagement.Application.Contracts.Persistence;
+
+public interface ILeaveRequestRepository: IGenericRepository<LeaveRequest>
+{
+    
+}
