1975d612bdbf8f9475346cea33e4f5f9bcd63ed5
Ahmet Altun <ahmet.altun60@gmail.com>
2023-02-03 10:55:32 +0300

    Setup mocks

---
+++ test/HRLeaveManagement.UnitTests/Mocks/MockLeaveRepository.cs
@@ -0,0 +1,43 @@
+﻿using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Domain;
+using Moq;
+
+namespace HRLeaveManagement.UnitTests.Mocks;
+
+public class MockLeaveRepository
+{
+    public static Mock<ILeaveTypeRepository> GetLeaveTypeMockLeaveTypeRepository()
+    {
+        var leaveTypes = new List<LeaveType>
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                DefaultDays = 10,
+                Name = "Test Vacation"
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                DefaultDays = 20,
+                Name = "Test Maternity"
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                DefaultDays = 15,
+                Name = "Test Stic"
+            }
+        };
+
+        var mockRepo = new Mock<ILeaveTypeRepository>();
+        mockRepo.Setup(r => r.GetAllAsync(default, null)).Returns(() => leaveTypes);
+        mockRepo.Setup(r => r.CreateAsync(It.IsAny<LeaveType>(), default)).Returns((LeaveType leaveType) =>
+        {
+            leaveTypes.Add(leaveType);
+            return Task.CompletedTask;
+        });
+
+        return mockRepo;
+    }
+}
